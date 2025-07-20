9ee7d56eba7748003c2ec21b2680a228cb7ceb8f
Eoin Brennan <eoin264@hotmail.com>
2025-07-20 14:56:51 +0100

    Initial commit of Android code

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        flatDir {
+            dirs("app/libs")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MayeosGlyphToys"
+include(":app")
